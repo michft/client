83f2af3c5c4e1b2c0ffe1a99c3f324989340bb40
Max Goodman <c@chromakode.com>
2016-10-26 15:36:44 -0700

    add profile route to search

--- shared/search/routes.js
+++ shared/search/routes.js
@@ -1,9 +1,13 @@
 // @flow
 import {Routes} from '../route-tree'
+import profileRoutes from '../profile/routes'
 import Search from './'
 
 const routeTree = Routes({
   component: Search,
+  children: {
+    profile: profileRoutes,
+  }
 })
 
 export default routeTree
